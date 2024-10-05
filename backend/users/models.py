364579249bd02f8c71d4140c302b61ba4f726133
Adhishri Karbhari <tawheed.chilwan55@gmail.com>
2024-10-05 11:26:11 +0530

    working on user profile

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,4 +6,12 @@ class CustomUser(AbstractUser):
     phone_number = models.IntegerField(max_length=15, blank=True, null=True)
     def __str__(self):
         return self.username
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
     
